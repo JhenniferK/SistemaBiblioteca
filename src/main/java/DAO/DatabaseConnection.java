761c0a2b2af1df71193fb6c6aafc420511ee132b
Jhennifer Kelly Nicolau da Cunha <132682121+JhenniferK@users.noreply.github.com>
2025-03-05 17:33:37 -0300

    Update Conexão BD

--- src/main/java/DAO/DatabaseConnection.java
+++ src/main/java/DAO/DatabaseConnection.java
@@ -1,16 +1,31 @@
 package DAO;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://ep-tight-art-a5u0hz7v-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require";
-    private static final String USER = "neondb_owner";
-    private static final String PASSWORD = "npg_3HNBAMayPo4X";
+    private static final String CONFIG_FILE = "config.properties";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(CONFIG_FILE));
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar configurações do banco de dados", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
